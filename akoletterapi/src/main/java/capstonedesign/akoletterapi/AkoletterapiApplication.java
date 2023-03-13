7c512492986414326577a1a78efd62aaceca90bb
Hyunjun Harry Yim <harryyim707@gmail.com>
2023-03-13 19:45:20 +0900

    FIRST COMMIT

---
+++ akoletterapi/src/main/java/capstonedesign/akoletterapi/AkoletterapiApplication.java
@@ -0,0 +1,13 @@
+package capstonedesign.akoletterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkoletterapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkoletterapiApplication.class, args);
+	}
+
+}
---
+++ akoletterapi/src/test/java/capstonedesign/akoletterapi/AkoletterapiApplicationTests.java
@@ -0,0 +1,13 @@
+package capstonedesign.akoletterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkoletterapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
