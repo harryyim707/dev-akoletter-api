ac1ee05395b286b50fde50bf20ffc901b288a01a
Hyunjun Harry Yim <harryyim707@gmail.com>
2023-03-17 17:10:35 +0900

    change: 프로젝트 재설정

---
+++ src/main/java/akoletter/devakoletterapi/DevAkoletterApiApplication.java
@@ -0,0 +1,13 @@
+package akoletter.devakoletterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAkoletterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevAkoletterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/akoletter/devakoletterapi/DevAkoletterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package akoletter.devakoletterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevAkoletterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
