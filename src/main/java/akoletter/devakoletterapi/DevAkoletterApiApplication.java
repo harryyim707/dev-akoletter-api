dec1837d63309f42812d4f2a9c6dabc164818925
sihyun99 <fkrtldn1@naver.com>
2023-05-15 14:27:48 +0900

    Revert "feat: postlist 수정"
    This reverts commit aa0728d16411ea467a576bdcbdf4578ee440c73c.

--- src/main/java/akoletter/devakoletterapi/DevAkoletterApiApplication.java
+++ src/main/java/akoletter/devakoletterapi/DevAkoletterApiApplication.java
@@ -2,10 +2,7 @@ package akoletter.devakoletterapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -14,13 +11,5 @@ public class DevAkoletterApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DevAkoletterApiApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3001");
-			}
-		};
-	}
+
 }
