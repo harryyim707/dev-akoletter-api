dc291d2ea7ff2051ea7e0b6026e4d8d6a2c8bd17
Hyunjun Harry Yim <harryyim707@gmail.com>
2023-06-08 18:06:26 +0900

    fix(properties)

--- src/main/java/akoletter/devakoletterapi/post/service/PostServiceImpl.java
+++ src/main/java/akoletter/devakoletterapi/post/service/PostServiceImpl.java
@@ -36,7 +36,7 @@ public class PostServiceImpl implements PostService {
   private final PostMstRepository postMstRepository;
   private final FileMstRepository fileMstRepository;
   private final MemberMstRepository memberMstRepository;
-  @Value("${CUSTOMCONNSTR_defaultImageId}")
+  @Value("${defaultImageId}")
   int defaultImageId;
 
 
