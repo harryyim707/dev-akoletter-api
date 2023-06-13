ce14edd8020b1f119c053a0a7a2edab947bcdf79
sihyun99 <fkrtldn1@naver.com>
2023-06-13 17:39:26 +0900

    feat: CORS 필터 수정

--- src/main/java/akoletter/devakoletterapi/util/File/config/WebMvcConfiguration.java
+++ src/main/java/akoletter/devakoletterapi/util/File/config/WebMvcConfiguration.java
@@ -1,6 +1,9 @@
 package akoletter.devakoletterapi.util.File.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +13,22 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("file:///C:/Users/1/IdeaProjects/img_test/");
+    }
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://nojaewon.github.io/")
+                .exposedHeaders("jwt-token")
+                .allowedHeaders("Content-Type","X-AUTH-TOKEN","Authorization");
+        ;
+
+    }
+
+    public void configure(HttpSecurity http) throws Exception{
+        http.httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeHttpRequests();
+
     }
 }
